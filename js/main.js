e494dc2417bc7c693a20985d8ad82130263d9e09
efs0-cod3 <eflearnng@gmail.com>
2022-04-20 18:09:02 -0400

    getting DRY

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ const sMenu = document.getElementById('menu_icon');
 const menu = document.getElementById('show-menu');
 const navigation = document.getElementsByClassName('nav');
 const btn = document.getElementById('submit');
-let fName = document.getElementById('fullName')
-let message = document.getElementById('message')
-let email = document.getElementById('email_id')
+const fName = document.getElementById('fullName')
+const form_message = document.getElementById('message')
+const email = document.getElementById('email_id')
 
 
 // activate and deactivate menu button
@@ -39,13 +39,13 @@ document.querySelector('#form').addEventListener('submit', form)
 function form(e) {
   e.preventDefault()
 
-  if (fName.value == '' && message.value == '' && email.value == '') {
+  if (fName.value == '' && form_message.value == '' && email.value == '') {
     fName.classList.add('fill-warning');
     fName.setAttribute('placeholder', 'Please fill this box');
     email.classList.add('fill-warning');
     email.setAttribute('placeholder', 'Please fill this box');
-    message.classList.add('fill-warning');
-    message.setAttribute('placeholder', 'Please fill this box');
+    form_message.classList.add('fill-warning');
+    form_message.setAttribute('placeholder', 'Please fill this box');
     setTimeout(removeWarning, 2000);
 
   } else if (fName.value == '' && email.value == '') {
@@ -55,18 +55,18 @@ function form(e) {
     email.setAttribute('placeholder', 'Please fill this box');
     setTimeout(removeWarning, 2000);
 
-  } else if (fName.value == '' && message.value == '') {
+  } else if (fName.value == '' && form_message.value == '') {
     fName.classList.add('fill-warning');
     fName.setAttribute('placeholder', 'Please fill this box');
-    message.classList.add('fill-warning');
-    message.setAttribute('placeholder', 'Please fill this box');
+    form_message.classList.add('fill-warning');
+    form_message.setAttribute('placeholder', 'Please fill this box');
     setTimeout(removeWarning, 2000);
 
-  } else if (message.value == '' && email.value == '') {
+  } else if (form_message.value == '' && email.value == '') {
     email.classList.add('fill-warning');
     email.setAttribute('placeholder', 'Please fill this box');
-    message.classList.add('fill-warning');
-    message.setAttribute('placeholder', 'Please fill this box');
+    form_message.classList.add('fill-warning');
+    form_message.setAttribute('placeholder', 'Please fill this box');
     setTimeout(removeWarning, 2000);
 
   } else if (fName.value == '') {
@@ -79,34 +79,29 @@ function form(e) {
     email.setAttribute('placeholder', 'Please fill this box');
     setTimeout(removeWarning, 2000);
 
-  } else if (message.value == '') {
-    message.classList.add('fill-warning');
-    message.setAttribute('placeholder', 'Please fill this box');
+  } else if (form_message.value == '') {
+    form_message.classList.add('fill-warning');
+    form_message.setAttribute('placeholder', 'Please fill this box');
     setTimeout(removeWarning, 2000);
 
   } else {
     btn.value = 'Sending...';
-    sendMail()
+    let parameters = {
+      from_name: fName.value,
+      email_id: email.value,
+      message: form_message.value,
+    }
+    emailjs.send('service_quuge9h', 'template_4tf2otb', parameters)
+      .then(function (res) {
+        alert(`Thanks ${fName.value}, your message was sent`)
+        btn.value = "Send Message"
+        fName.value = ''
+        email.value = ''
+        form_message.value = ''
+      })
 
   };
 }
-
-function sendMail() {
-  let parameters = {
-    from_name: document.getElementById('fullName').value,
-    email_id: document.getElementById('email_id').value,
-    message: document.getElementById('message').value,
-  }
-  emailjs.send('service_quuge9h', 'template_4tf2otb', parameters)
-    .then(function (res) {
-      alert(`Thanks ${fName.value}, your message was sent`)
-      btn.value = "Send Message"
-      fName.value = ''
-      email.value = ''
-      message.value = ''
-    })
-}
-
 
 function removeWarning() {
   fName.classList.remove('fill-warning');
@@ -115,5 +110,4 @@ function removeWarning() {
   message.setAttribute('placeholder', 'Send a message and let\'s schedule a chat!');
   email.setAttribute('placeholder', 'Email here...');
   fName.setAttribute('placeholder', 'Name here...');
-
 }
